901055e472448e1fd7ab22bc39d86ededef4f87a
Happy Guleria <happyguleria123@gmail.com>
2021-03-15 14:06:00 +0530

    worked on customer flow

---
+++ Gahir Agro/Controllers/Customer/Home/CustomerSearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  CustomerSearchVC.swift
+//  Gahir Agro
+//
+//  Created by Apple on 15/03/21.
+//
+
+import UIKit
+
+class CustomerSearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+}
